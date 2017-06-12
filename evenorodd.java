b8aa7b5707006fc72d1cbacf2580d48745f6abd6
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-06-12 15:16:49 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class evenorodd {
+	public static void main(String[] args) {
+	 int number;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Given Number");
+	 number=sc.nextInt();
+	 if((number%2)==0) {
+		 System.out.println("The given number is even");
+                           }
+	 else {
+	 System.out.println("The given number is odd");
+                           }
+	             }
+                 }
