bc73b62dd898c85edcac4ea784f40582f15d24c6
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-03 18:13:45 +0530

    Add files via upload

---
+++ Evennumprint.java
@@ -0,0 +1,15 @@
+package guvi;
+
+public class Evennumprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10;i<20;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
