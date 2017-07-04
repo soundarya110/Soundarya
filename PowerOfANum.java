99681bad8037f76a41594fde11d266cc459edab0
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-04 11:24:34 +0530

    Add files via upload

---
+++ PowerOfANum.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class PowerOfANum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int pow=sc.nextInt();
+		System.out.println("The power is:"+Math.pow(num, pow));
+
+	}
+
+}
