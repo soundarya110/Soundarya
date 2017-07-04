6299418e658b13f9ec1b8808b72c7c7cf567e5a6
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-04 11:20:14 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+package guvi;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=2;
+		for(int i=1;i<=10;i++){
+			System.out.println(i+"*"+num+"="+(i*num));
+		}
+
+	}
+
+}
