29f0af79927ffbbbd2901a27d67564d498b339f4
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-04 11:59:29 +0530

    Add files via upload

---
+++ SumLessThanGivenNum.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.Scanner;
+public class SumLessThanGivenNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=num;i++){
+			sum=sum+i;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
