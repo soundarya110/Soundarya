e282ce7855496dbe79f4899a84fc9aeabea0e4ce
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-05 13:58:09 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,12 @@
+package player;
+import java.util.Scanner;
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		System.out.println((Integer.parseInt(str)));
+	}
+
+}
