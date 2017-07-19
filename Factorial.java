553cbd6ae3a06600e89192b5bdc89da6db6499f7
soundarya110 <soundarya.bece@ksrce.ac.in>
2017-07-19 13:52:00 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,16 @@
 package player;
-
+import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int factorial=1;
+		for(int i=1;i<=n;i++){
+			factorial=factorial*i;
+		}
+		System.out.println(factorial);
 
 	}
 
